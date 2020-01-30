c2448c8a9172814a99f0d30c538333c562f95fe9
jakazzy <jakazzy@gmail.com>
2020-01-30 13:08:39 +0000

    add server

--- src/controllers/v0/feed/routes/feed.router.ts
+++ src/controllers/v0/feed/routes/feed.router.ts
@@ -1,14 +1,69 @@
 import { Router, Request, Response } from 'express'
 import { FeedItem } from '../models/FeedItem'
 import { requireAuth } from '../../users/routes/auth.router'
-// import * as AWS from '../../../../aws'
+import * as AWS from '../../../../aws'
 
 const router: Router = Router()
 
 // Get all feed items
+router.get('/', async(req:Request, res:Response)=>{
+    const items = await FeedItem.findAndCountAll({order:[['id', 'DESC']]})
+    console.log(items, 'i want to see the rows')
+    items.rows.map((item)=>{
+        if(item.url){
+            item.url = AWS.getGetSignedUrl(item.url)
+        }
+    })
+    res.send(items)
+})
 
 // Ger a specific resource by primary key
+router.get('/:id', async(req:Request, res: Response)=>{
+    const { id } = req.params
+    const item = await FeedItem.findByPk(id)
+    if(!item){return res.status(400).send({message: "Item cannot be found"})}
+    return res.status(200).send(item)
+})
 
 // update a specific resource
+router.patch('/:id', async(req:Request, res:Response)=>{
+    const { id } = req.params
+    if(!id) { return res.status(400).send({message: 'Item is required'})}
+    const item =await FeedItem.update({url:'this is a url'}, {where: {id:id}})
+    return res.status(200).send({message:'url successfully updated'})
+})
 
 // Get a signedurl to put a new item in the bucket 
+router.get('/signed-url/:filename', requireAuth, async(req:Request, res:Response)=>{
+
+    const { filename }= req.params
+    const url = AWS.getPutSignedUrl(filename)
+    res.status(201).send({url:url})
+})
+
+// Post metadata and the filename after the file is uploaded
+// Note the file name is the key name in the s3 bucker
+// body:{caption:string, filename: string}
+
+router.post('/', requireAuth, async(req: Request, res:Response)=>{
+    const caption = req.body.caption
+    const fileName = req.body.url
+
+    // check caption is valid
+    if (!caption){
+        return res.status(400).send({message: 'Caption is required or malformed'})
+    }
+
+    if(!fileName){
+        return res.status(400).send({ message: 'File url is required'})
+    }
+
+    const item = await new FeedItem({
+        caption:caption,
+        url:fileName
+    })
+    const saved_item = await item.save()
+    saved_item.url = AWS.getGetSignedUrl(saved_item.url)
+    return res.status(201).send(saved_item)
+})
+export const FeedRouter: Router = router;
--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,32 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response} from 'express'
+import { sequelize} from './sequelize'
 import bodyParser from 'body-parser'
+import { IndexRouter } from './controllers/v0/index.router'
+import { V0MODELS } from './controllers/v0/model.index'
+import {NextFunction } from 'connect'
 
-const app = express()
-const port = 8082
+(async ()=>{
+    await sequelize.addModels(V0MODELS)
+    await sequelize.sync()
 
-app.use(bodyParser.json())
-app.get('/', (req: Request, res:Response)=>{
-    res.send('welcome to the application')
-})
+    const app = express()
+    const port = process.env.PORT || 8080
+    const restrictCors=(req:Request, res:Response, next:NextFunction)=>{
+        res.header("Access-Control-Allow-Origin", "http://localhost:8100")
+        res.header("Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization")
+        next()
+    }
 
-app.get('/api/v0/feeds', (req: Request, res:Response)=>{
-    res.send('Provide the details for all the feeds')
-})
+    app.use(bodyParser.json())
+    app.use(restrictCors)
+    app.use('/api/v0', IndexRouter)
 
-app.get('/api/v0/feeds/:id', (req:Request, res:Response)=>{
-    const {id }= req.params
+    app.get('/', async(req:Request, res:Response)=>{
+        res.send("api/v0")
+    })
 
-    res.send(`Provide details with feed of number ${id}`)
-})
-app.patch('/api/v0/feeds/:id', (req:Request, res:Response)=>{
-    const {id }= req.params
-
-    res.send(`Provide details with feed of number ${id}`)
-})
-
-app.post('/api/v0/feeds', (req:Request, res:Response)=>{
-    res.send('Post this data')
-})
-
-app.get('/signed-url/:fileName', 
- 
-    async (req: Request, res: Response) => {})
-app.listen(port, ()=>{
-    console.log('app running at port number 8082')
-    console.log('shutdown server using CTRL + C')
-})
-
+    app.listen(port, ()=>{
+        console.log( `server running http://localhost:${ port }` );
+        console.log( `press CTRL+C to stop server` );
+    })
+})()
