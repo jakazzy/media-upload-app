000015e20ed03b2728a2f2b1b8e274553f3a1777
jakazzy <jakazzy@gmail.com>
2020-01-30 03:49:34 +0000

    add config file

---
+++ src/feed.ts
@@ -0,0 +1,25 @@
+export const feeds = [
+   
+    {
+        "id": 1,
+        "caption": "xanderina",
+        "url": "https://s3.eu-west-2.amazonaws.com/",
+        "createdAt": "2020-01-24T09:50:28.138Z",
+        "updatedAt": "2020-01-24T09:50:28.139Z"
+    },
+
+    {
+        "id": 2,
+        "caption": "xanderia",
+        "url": "https://s3.eu-west-2.amazonaws.com/",
+        "createdAt": "2020-01-24T09:50:28.138Z",
+        "updatedAt": "2020-01-24T09:50:28.139Z"
+    },
+    {
+        "id": 3,
+        "caption": "xander",
+        "url": "https://s3.eu-west-2.amazonaws.com/",
+        "createdAt": "2020-01-24T09:50:28.138Z",
+        "updatedAt": "2020-01-24T09:50:28.139Z"
+    }
+]
--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,28 @@ app.get('/', (req: Request, res:Response)=>{
     res.send('welcome to the application')
 })
 
+app.get('/api/v0/feeds', (req: Request, res:Response)=>{
+    res.send('Provide the details for all the feeds')
+})
 
+app.get('/api/v0/feeds/:id', (req:Request, res:Response)=>{
+    const {id }= req.params
 
+    res.send(`Provide details with feed of number ${id}`)
+})
+app.patch('/api/v0/feeds/:id', (req:Request, res:Response)=>{
+    const {id }= req.params
+
+    res.send(`Provide details with feed of number ${id}`)
+})
+
+app.post('/api/v0/feeds', (req:Request, res:Response)=>{
+    res.send('Post this data')
+})
+
+app.get('/signed-url/:fileName', 
+ 
+    async (req: Request, res: Response) => {})
 app.listen(port, ()=>{
     console.log('app running at port number 8082')
     console.log('shutdown server using CTRL + C')
